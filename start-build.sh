508a37b0d845b3d12825095757abc9572017efa8
Daniel <me.home@outlook.de>
2023-06-29 19:26:41 +0200

    linux build script

---
+++ start-build.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+dotnet publish ./src/Ve.Direct.InfluxDB.Collector/Ve.Direct.InfluxDB.Collector.csproj \
+	   --runtime linux-x64 \
+	   --self-contained false \
+	   -c Release \
+	   -v minimal \
+	   -o ./build \
+	   -p:PublishReadyToRun=false \
+	   -p:PublishSingleFile=true \
+	   -p:CopyOutputSymbolsToPublishDirectory=false \
+	   --nologo
