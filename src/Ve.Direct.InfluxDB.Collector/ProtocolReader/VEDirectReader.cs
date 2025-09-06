198068ba7e2bc1e1f27d32921fa8c94d2f853ea5
DocBrown101 <docbrown@tutanota.de>
2025-09-06 21:07:12 +0200

    cleanup

--- src/Ve.Direct.InfluxDB.Collector/ProtocolReader/VEDirectReader.cs
+++ src/Ve.Direct.InfluxDB.Collector/ProtocolReader/VEDirectReader.cs
@@ -10,14 +10,15 @@ namespace Ve.Direct.InfluxDB.Collector.ProtocolReader
     {
         private readonly Dictionary<string, string> serialData;
         private readonly string serialPortName;
-        private readonly char header1;
-        private readonly char header2;
-        private readonly char hexmarker;
-        private readonly char delimiter;
-        private string key;
-        private string value;
-        private ReadState state;
-        private byte bytes_sum;
+        private const char HEADER1 = '\r';
+        private const char HEADER2 = '\n';
+        private const char HEXMARKER = ':';
+        private const char DELIMITER = '\t';
+
+        private ReadState state = ReadState.WAIT_HEADER;
+        private byte bytes_sum = 0;
+        private string key = string.Empty;
+        private string value = string.Empty;
 
         private enum ReadState
         {
@@ -35,21 +36,13 @@ namespace Ve.Direct.InfluxDB.Collector.ProtocolReader
 
             ConsoleLogger.Info($"Using Port: {this.serialPortName}");
             ConsoleLogger.Info($"Collect Metrics ...");
-
-            this.header1 = '\r';
-            this.header2 = '\n';
-            this.hexmarker = ':';
-            this.delimiter = '\t';
-            this.key = "";
-            this.value = "";
-            this.bytes_sum = 0;
-            this.state = ReadState.WAIT_HEADER;
         }
 
         public bool ProcessInputByte(byte inputByte)
         {
             var inputByteAsChar = Convert.ToChar(inputByte);
-            if (inputByteAsChar == this.hexmarker && this.state != ReadState.IN_CHECKSUM)
+
+            if (inputByteAsChar == HEXMARKER && this.state != ReadState.IN_CHECKSUM)
             {
                 this.state = ReadState.HEX;
             }
@@ -62,12 +55,12 @@ namespace Ve.Direct.InfluxDB.Collector.ProtocolReader
             switch (this.state)
             {
                 case ReadState.WAIT_HEADER:
-                    if (inputByteAsChar == this.header1) this.state = ReadState.WAIT_HEADER;
-                    else if (inputByteAsChar == this.header2) this.state = ReadState.IN_KEY;
+                    if (inputByteAsChar == HEADER1) this.state = ReadState.WAIT_HEADER;
+                    else if (inputByteAsChar == HEADER2) this.state = ReadState.IN_KEY;
                     break;
 
                 case ReadState.IN_KEY:
-                    if (inputByteAsChar == this.delimiter)
+                    if (inputByteAsChar == DELIMITER)
                     {
                         this.state = this.key == "Checksum"
                             ? ReadState.IN_CHECKSUM
@@ -80,7 +73,7 @@ namespace Ve.Direct.InfluxDB.Collector.ProtocolReader
                     break;
 
                 case ReadState.IN_VALUE:
-                    if (inputByteAsChar == this.header1)
+                    if (inputByteAsChar == HEADER1)
                     {
                         this.state = ReadState.WAIT_HEADER;
                         if (this.serialData.ContainsKey(this.key))
@@ -111,7 +104,7 @@ namespace Ve.Direct.InfluxDB.Collector.ProtocolReader
 
                 case ReadState.HEX:
                     this.bytes_sum = 0;
-                    if (inputByteAsChar == this.header2) this.state = ReadState.WAIT_HEADER;
+                    if (inputByteAsChar == HEADER2) this.state = ReadState.WAIT_HEADER;
                     break;
 
                 default:
@@ -122,24 +115,22 @@ namespace Ve.Direct.InfluxDB.Collector.ProtocolReader
 
         public void ReadSerialPortData(Action<Dictionary<string, string>> callbackFunction, CancellationToken ct)
         {
-            using (var serialPort = new SerialPort(this.serialPortName, 19200))
+            using var serialPort = new SerialPort(this.serialPortName, 19200);
+            serialPort.ReadTimeout = 5000;
+            serialPort.Open();
+
+            while (!ct.IsCancellationRequested)
             {
-                serialPort.ReadTimeout = 5000;
-                serialPort.Open();
-
-                while (!ct.IsCancellationRequested)
+                var inputByte = (byte)serialPort.ReadByte();
+                if (inputByte == 0)
                 {
-                    var inputByte = (byte)serialPort.ReadByte();
-                    if (inputByte == 0)
-                    {
-                        continue;
-                    }
+                    continue;
+                }
 
-                    var allBytesReceived = this.ProcessInputByte(inputByte);
-                    if (allBytesReceived)
-                    {
-                        callbackFunction(this.serialData);
-                    }
+                var allBytesReceived = this.ProcessInputByte(inputByte);
+                if (allBytesReceived)
+                {
+                    callbackFunction(this.serialData);
                 }
             }
         }
